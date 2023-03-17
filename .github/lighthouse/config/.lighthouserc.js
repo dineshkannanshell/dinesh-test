5f252f2c7226047e558525512e242b0952a3eb58
Dinesh <99355801+dineshkannanshell@users.noreply.github.com>
2023-03-17 11:23:46 +0530

    adding assertions

--- .github/lighthouse/config/.lighthouserc.js
+++ .github/lighthouse/config/.lighthouserc.js
@@ -1,7 +1,13 @@
 module.exports = {
   ci: {
     assert: {
-      preset: 'lighthouse:recommended',
+      "assertions": {
+        "first-contentful-paint": ["warn", {"maxNumericValue": 4000}],
+        "viewport": "error",
+        "resource-summary:document:size": ["error", {"maxNumericValue": 14000}],
+        "resource-summary:font:count": ["warn", {"maxNumericValue": 1}],
+        "resource-summary:third-party:count": ["warn", {"maxNumericValue": 5}]
+      },
     },
     collect: {
       url: ['https://npe20.shell.co.uk/'],
